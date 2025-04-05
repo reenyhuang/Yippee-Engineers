4184472ecacd577754ab2955338fbebd77cc252e
SGrimsley02 <sam.grimsley@gmail.com>
2025-04-05 10:52:15 -0500

    Got collisions working, but movement currently jerky

--- code/main.py
+++ code/main.py
@@ -11,10 +11,15 @@ WINDOW_HEIGHT = 600
 
 BG_COLOR = (222, 235, 255)
 
+forward_keys = (pygame.K_UP, pygame.K_w)
+backward_keys = (pygame.K_DOWN, pygame.K_s)
+left_keys = (pygame.K_LEFT, pygame.K_a)
+right_keys = (pygame.K_RIGHT, pygame.K_d)
+
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.key.set_repeat(500, 800)
+        pygame.key.set_repeat(500, 100)
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Yippee Enginneer")
         self.clock = pygame.time.Clock()
@@ -32,7 +37,7 @@ class Game:
     def new(self):
         self.all_sprites = pygame.sprite.Group()
         self.walls = pygame.sprite.Group()
-        self.player = Player("RoundLili.png", 0, 0)
+        self.player = Player("RoundLili.png", 0, 0, self)
         for x in range(10, 20):
             Wall(self, x, 5)
 
@@ -51,14 +56,25 @@ class Game:
         for sprite in sprites: ## Also draw all sprites? I think we're keeping track of some sprites in two different places
             sprite.draw(self.screen)
 
-    def events(self):
+    def events(self): ## Key press events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
-                keys_down.add(event.key)
+                
+                if event.key in forward_keys:
+                    self.player.move(dy=-1)
+                elif event.key in backward_keys:
+                    self.player.move(dy=1)
+                elif event.key in left_keys:
+                    self.player.move(dx=-1)
+                elif event.key in right_keys:
+                    self.player.move(dx=1)
+                elif event.key == pygame.K_ESCAPE:
+                    self.running = False
             elif event.type == pygame.KEYUP:
-                keys_down.remove(event.key)
+                pass
+            
 
     def draw_map(self):
         self.screen.fill(BG_COLOR)
--- code/player.py
+++ code/player.py
@@ -4,44 +4,59 @@ from input import is_key_pressed
 from os import path
 from settings import TILE_SIZE
 
-movement_speed = 1
+movement_speed = TILE_SIZE
 rotation_speed = 1
 
 class Player(Sprite):
-    def __init__(self, image, x, y):
+    def __init__(self, image, x, y, game):
         super().__init__(image, x, y)
         self.original_image = self.image
         self.angle = 0
+        self.game = game
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        
 
+    def bump_into_something(self, dx=0, dy=0):
+        future_rect = self.rect.copy()
+        future_rect.x += dx * TILE_SIZE
+        future_rect.y += dy * TILE_SIZE
+        for wall in self.game.walls:
+            if future_rect.colliderect(wall.rect):
+                return True
+        return False
+
+    def move(self, dx=0, dy=0):
+        
+        new_x = self.x + dx*movement_speed
+        new_y = self.y + dy*movement_speed
+
+        future_rect = self.rect.copy()
+        future_rect.x = new_x
+        future_rect.y = new_y
+
+        if not any(future_rect.colliderect(wall.rect) for wall in self.game.walls):
+            self.x = new_x
+            self.y = new_y
+            self.rect.topleft = (self.x, self.y)
+
+        #if dx != 0 or dy != 0: ## Pretty sure this if statement is not necessary, the direction checks same thing
+        direction = dx if dx != 0 else dy
+        self.angle += rotation_speed * direction
+
+        self.image = pygame.transform.rotate(self.original_image, self.angle)
+        self.rect.x, self.rect.y = self.x * TILE_SIZE, self.y * TILE_SIZE
+
+        #else:
+            #self.image = self.original_image
+            #self.rect = self.image.get_rect(center=(self.x, self.y))
 
     def update(self):
-        dx, dy = 0, 0
-        forward = pygame.K_UP or pygame.K_w
-        backward = pygame.K_DOWN or pygame.K_s
-        left = pygame.K_LEFT or pygame.K_a
-        right = pygame.K_RIGHT or pygame.K_d
-        if is_key_pressed(forward):
-            dy -= movement_speed
-        if is_key_pressed(backward):
-            dy += movement_speed
-        if is_key_pressed(left):
-            dx -= movement_speed
-        if is_key_pressed(right):
-            dx += movement_speed
+        self.move()
+        self.rect.x = self.x * TILE_SIZE
+        self.rect.y = self.y * TILE_SIZE
 
-        self.x += dx
-        self.y += dy
 
-        if dx != 0 or dy != 0:
-            direction = -1 if dx < 0 or dy < 0 else 1 if dx > 0 or dy > 0 else 0
-            self.angle += rotation_speed * direction
-
-            self.image = pygame.transform.rotate(self.original_image, self.angle)
-            self.rect = self.image.get_rect(center=(self.x, self.y))
-
-        else:
-            self.image = self.original_image
-            self.rect = self.image.get_rect(center=(self.x, self.y))
+        
 
 
 
