d501d95544045e16bd7bfd83a673d77daf537b99
SGrimsley02 <sam.grimsley@gmail.com>
2025-04-05 19:07:42 -0500

    Added inventory items

--- code/inventory.py
+++ code/inventory.py
@@ -5,9 +5,18 @@ class Inventory:
     def __init__(self):
         self.slots = []
 
-        self.image = pygame.image.load("Images/Inventory.png")
+        self.image = pygame.image.load("images/inventory.png")
         self.rect = self.image.get_rect()
         self.rect.topleft = (0, 160)
 
-        self.slots.append(InventorySlot("Images/coffee.png", (16, 60)))
-        self.slots.append(InventorySlot("Images/computer.png", (110, 360)))
+        self.slots.append(InventorySlot("images/coffee.png", (16, 176)))
+        self.slots.append(InventorySlot("images/computer.png", (16, 192)))
+        self.slots.append(InventorySlot("images/hw.png", (16, 208)))
+        self.slots.append(InventorySlot("images/charger.png", (16, 224)))
+        self.slots[1].count += 1
+        self.slots[3].count += 1
+    
+    def render(self, display):
+        display.blit(self.image, self.rect)
+        for slot in self.slots:
+            slot.render(display)
--- code/inventorySlot.py
+++ code/inventorySlot.py
@@ -3,7 +3,7 @@ import pygame
 class InventorySlot:
     def __init__(self, name, pos):
         self.image = pygame.image.load(name)
-        self.rect = self.image.get_rect ()
+        self.rect = self.image.get_rect()
         self.rect.topleft = pos
         self.count = 0
         self.font = pygame.font.Font(None, 25)
@@ -12,5 +12,3 @@ class InventorySlot:
         text = self.font.render(str(self.count), True, (0,0,0))
         display.blit(self.image, self.rect)
         display.blit(text, self.rect.midright)
-
-        
